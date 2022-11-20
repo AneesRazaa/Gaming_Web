759ecd9d8f65d185450680c61ee98aca4ec33971
Anees Raza <100798859+AneesRazaa@users.noreply.github.com>
2022-11-21 01:17:12 +0500

    gaming_commit

---
+++ data.js
@@ -0,0 +1,37 @@
+const CATEGORIES = [
+  {
+    id:1 ,
+    name:'indoor games'
+  },
+  {
+    id:2,
+    name:'outdoor Games'
+  }
+]
+const PRODUCTS = [
+    {
+      id: 1,
+      description: 'lorem ipsum 1',
+      img: 'call-of-duty-logo'
+    },
+    {
+      id: 2,
+      description: 'lorem ipsum 2',
+      img: 'logo-table-tennis'
+    },
+    {
+      id: 3,
+      description: 'lorem ipsum 3',
+      img: 'minecraft'
+    },
+    {
+      id: 4,
+      description: 'lorem ipsum 4 ',
+      img: 'pak logo cricket'
+    },
+    {
+      id: 5,
+      description: 'lorem ipsum 5',
+      img: 'swimming'
+    }
+  ];
